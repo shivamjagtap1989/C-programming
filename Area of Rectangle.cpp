5cc5a2b687dfc043748ae453d12463757543f0d2
shivamjagtap1989 <shivamjagtap1989@gmail.com>
2026-01-18 16:01:44 +0530

    Add files via upload

---
+++ Area of Rectangle.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+ {
+ 	int a;
+ 	int b;
+  printf("Enter the length\n");
+  scanf("%d",&a);
+  printf("Enter the Breadth\n");
+  scanf("%d", &b);
+  printf("The Area Of Rectangle is %d", a*b);
+  
+  return 0;
+}
