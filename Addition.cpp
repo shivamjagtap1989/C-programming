f55ac3485e95d91142fe870b51d934fc71b47c43
shivamjagtap1989 <shivamjagtap1989@gmail.com>
2026-01-18 16:01:00 +0530

    Add files via upload

---
+++ Addition.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int a, b, c;
+	
+	printf("Enter the First Number \n");
+	scanf("%d", &a);
+	
+	printf("Enter the Second Number \n");
+	scanf("%d", &b);
+	
+	c=a+b;
+	printf("Sum =  %d", c);
+
+}
